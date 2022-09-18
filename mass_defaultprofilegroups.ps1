7c0bf124eac1359afe67668cae6b2a103a7b7f6e
eneroh <28052446+eneroh@users.noreply.github.com>
2022-09-18 10:18:49 +1000

    Update mass_defaultprofilegroups.ps1

--- mass_defaultprofilegroups.ps1
+++ mass_defaultprofilegroups.ps1
@@ -1,13 +1,13 @@
 $groups = "","","","$office`<group>"
 $user = Get-Content %USERPROFILE%\Desktop\<location of txt doc>
 $data = Get-Content %USERPROFILE%\Desktop\<location of txt doc>
-#group = Get-Content %USERPROFILE%\Desktop\groups.txt
+#$group = Get-Content %USERPROFILE%\Desktop\groups.txt
 $office = Read-Host -Prompt 'Input the office: '
 foreach($user in $data) {
   Add-ADPrincipalGroupMembership $user -memberof $groups
 }
 
-#group is there as a static way to add groups, adding groups can simply be done via this powershell script too, make sure to uncomment the 
+#$group is there as a static way to add groups, adding groups can simply be done via this powershell script too, make sure to uncomment the 
 #group of course if you prefer the latter.
 #This script is a larger part of a whole I am working on. Further automating the process for new user creation for on-prem and potentially on-site.
 #The script pointing to a user txt doc also means these user groups can be applied en masse (french for on mass ;))
