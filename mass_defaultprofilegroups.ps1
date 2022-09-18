aa4a779ed0e4827df1aeaa065829906fd0866c9f
eneroh <28052446+eneroh@users.noreply.github.com>
2022-09-19 07:49:40 +1000

    Update mass_defaultprofilegroups.ps1

--- mass_defaultprofilegroups.ps1
+++ mass_defaultprofilegroups.ps1
@@ -8,7 +8,7 @@ foreach($user in $data) {
 }
 
 #$group is there as a static way to add groups, adding groups can simply be done via this powershell script too, make sure to uncomment the 
-#group of course if you prefer the latter.
-#This script is a larger part of a whole I am working on. Further automating the process for new user creation for on-prem and potentially on-site.
+#group of course if you prefer the former.
+#This script is a larger part of a whole I am working on. Further automating the process for new user creation for on-prem and potentially cloud.
 #The script pointing to a user txt doc also means these user groups can be applied en masse (french for on mass ;))
 #The script also has user input to specify an office location, if your AD groups contain multiple office locations of course
