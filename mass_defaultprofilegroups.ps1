59bd7c48496b43d175775dcce10516d604df357c
eneroh <28052446+eneroh@users.noreply.github.com>
2022-09-18 10:06:10 +1000

    Update mass_defaultprofilegroups.ps1

--- mass_defaultprofilegroups.ps1
+++ mass_defaultprofilegroups.ps1
@@ -6,3 +6,7 @@ $office = Read-Host -Prompt 'Input the office: '
 foreach($user in $data) {
   Add-ADPrincipalGroupMembership $user -memberof $groups
 }
+
+#group is there as a static way to add groups, adding groups can simply be done via this powershell script too, make sure to uncomment the #group of course.
+#This script is a larger part of a whole I am working on. Further automating the process for new user creation for on-prem and potentially on-site.
+#more to come ;)
