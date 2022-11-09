cb60eb323f6a182427fa1cc07f41f84db8968389
eneroh <28052446+eneroh@users.noreply.github.com>
2022-11-09 22:23:35 +1100

    Update createHomeDirv2.ps1

--- createHomeDirv2.ps1
+++ createHomeDirv2.ps1
@@ -1,8 +1,11 @@
-# Default perms for entire folder structuring, great for systems administrators building a ground up on-prem file sol.
+# Default perms for entire folder structuring, great for systems administrators building a ground up on-prem file perms sol.
 
 --- Coming later ---
 
 _____________________________
+
+# createHomeDirv2.ps1
+# Don't ask about v1 it's not as good and made by an entirely different person *cough*
 
 # Simpler honed in file permission set-up for those that already have a default file/folder permissions ACL base
 
@@ -19,18 +22,18 @@ $acl = Get-Acl $dir
 New-Item -ItemType directory -Path $dir
 Set-ADUser -Identity $user -HomeDirectory ($user) -HomeDrive "H:"
 
-# Give domain\user full access to individual folder, as well as permissions to relevant parties i.e. already present ACLs
+# Give domain\user full access to individual folder, as well as presents permissions to relevant parties i.e. default accounts and already present ACLs
 $fileSystemAccessFule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $permission
 $acl.SetAccessRule($fileSystemAccessRule)
 $acl.SetOwner($object)
 
-# Inherit permissions from parent folder
+# Inherit permissions from parent folder (not as useful)
 #$acl.SetACcessRuleProtection($true, $false)
 
-# Copy perms from another folder to specified (may be useful later
+# Copy perms from another folder to specified (may be useful later)
 #Get-Acl $parent | Set-Acl -path $dir
 
-#Apply the folder permission
+# Apply the updated folder permissions
 $acl | Set-Acl -Path $dir
 
 # Set-up smb sharing and permissions as per procedure
@@ -38,8 +41,8 @@ New-SmbShare -Name $user$ -Path $dir -FullAccess $user
 ________________________________
 # Shoutouts/Inspiration
 
-# shoutout to doc: (helped me gain a srs understanding)
+# shoutout to doc: (helped me gain a srs understanding around powershell acls)
 # https://ss64.com/ps/set-acl.html
 
-# shoutout to doc: (helped me understand like 2 lines (smbshares aha)
+# shoutout to doc: (helped me understand like 1 line specifically line 37 smbshares aha)
 # https://mcpmag.com/articles/2017/07/13/creating-shares-in-windows-using-the-smbshare-module.aspx
