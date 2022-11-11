205cfee6e8349558f95dc37fb1b3ce44987666b6
eneroh <28052446+eneroh@users.noreply.github.com>
2022-11-11 20:44:58 +1100

    Update and rename input_delete_custom_roam_drive to inputDeleteCustomHomeDrive.ps1

--- inputDeleteCustomHomeDrive.ps1
+++ inputDeleteCustomHomeDrive.ps1
@@ -1,3 +1,6 @@
+# inputDeleteCustomHomeDrive.ps1
+# Delete singular inputted user's home drive
+
 $user = Read-Host -Prompt 'Input the user name'
 $listFolder = '[Base Directory where del folder is located]'
 $path = $listFolder $user
