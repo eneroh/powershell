8ebe3ab982c92bf4f2f68e77e5e0720b4c427948
eneroh <28052446+eneroh@users.noreply.github.com>
2022-11-11 20:25:01 +1100

    Update mass_defaultprofilegroupsv2.ps1

--- mass_defaultprofilegroupsv2.ps1
+++ mass_defaultprofilegroupsv2.ps1
@@ -5,10 +5,14 @@ $data = Get-Content %USERPROFILE%\Desktop\<location of txt doc>
 #$group = Get-Content %USERPROFILE%\Desktop\groups.txt
 
 #location specific statements (easy expandability)
-$office = Read-Host -Prompt 'Input the office (Office Locations): '
+#$officeINPUT = Read-Host -Prompt 'Input the office (Office Locations): '
+$office = Get-ADUser -Identity $user -Properties City | Select-Object -ExpandProperty City
+$state = Get-ADUser -Identity $user -Properties State | Select-Object -ExpandProperty State
 $locgroup = "<location based group>","<location based group>"
 
 foreach($user in $data) {
+  Set-ADUser -Identity $user -Company "<Company Name>" -Office $state
+
   Add-ADPrincipalGroupMembership $user -memberof $groups
   
   if ($office -eq '<location>') {
