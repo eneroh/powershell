ce9a3a8a3e66415051689748ae7f7b4d8f108a48
eneroh <28052446+eneroh@users.noreply.github.com>
2022-11-11 20:55:46 +1100

    Update and rename mass_delete_custom_roam to massDeleteCustomHomeDrivev1.ps1

--- massDeleteCustomHomeDrivev1.ps1
+++ massDeleteCustomHomeDrivev1.ps1
@@ -1,3 +1,6 @@
+# massDeleteCustomHomeDrivev1.ps1
+# Script is a precursor to massDeleteCustomHomeDrivev2.ps1
+
 $data = Get-Content [location of text file to place names for action]
 $destination = '[Location of del/bin folder]'
 $line = Get-Content [location of text file to place names for action] (can be same place as $data)
