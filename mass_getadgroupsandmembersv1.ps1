35f3670f3668c9d37a90f330ef820d41cf269bc2
eneroh <28052446+eneroh@users.noreply.github.com>
2025-08-14 14:26:06 +1000

    Create mass_getadgroupsandmembersv1.ps1

---
+++ mass_getadgroupsandmembersv1.ps1
@@ -0,0 +1,13 @@
+# mass_getadgroupsandmembersv1.ps1
+# this script does the goal I wanted, however  didnt add a new line in between groups and its relevant members
+# formatting wasn't to my liking
+# fixed with v2 and chatgpt
+
+$groups = Get-Content "C:\temp\export.txt" | Select-Object -Skip 3
+
+$data = foreach($group in $groups)
+{
+  Get-ADGroup -Identity "$groups" | Select-Object -Property Name | Sort Name
+  Get-ADGroupMember -Identity "$groups" | Select-Object -Property Name | Sort Name
+}
+$data | Export-CSV -Path "C:\temp\final.csv" -NoTypeInformation
